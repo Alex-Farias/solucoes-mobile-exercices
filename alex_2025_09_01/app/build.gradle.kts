9952deb28c1c98a206c1a2559d32dfe3e80a0ae2
Alex Farias De Abreu Nabo <alex.15421@alunosatc.edu.br>
2025-09-01 20:55:45 -0300

    libs update

--- alex_2025_09_01/app/build.gradle.kts
+++ alex_2025_09_01/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.alex_2025_09_01"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.alex_2025_09_01"
